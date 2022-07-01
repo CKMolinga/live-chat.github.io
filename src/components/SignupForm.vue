d6c0f2295145e88e4e2aa058c4124d29e0957fc7
CKMolinga <ck.molinga@gmail.com>
2022-07-02 00:21:50 +0200

    created signup composable

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -20,14 +20,20 @@
 
 <script>
 import { ref } from "vue";
+
+import useSignup from "../composables/useSignup";
+
 export default {
   setup() {
+    const { error, signup } = useSignup();
+
     const displayName = ref("");
     const email = ref("");
     const password = ref("");
 
-    const handleSubmit = () => {
-      console.log(displayName.value, email.value, password.value);
+    const handleSubmit = async () => {
+      await signup(displayName.value, email.value, password.value);
+      console.log("You signed up!");
     };
 
     return { displayName, email, password, handleSubmit };
