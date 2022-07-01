5451adfeede718b6378b80c8f09b2d05a4ae6277
CKMolinga <ck.molinga@gmail.com>
2022-07-01 22:53:53 +0200

    setup firebase service

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAFK7FeB3F4DQRbNG9MUlZjPTQF7CQ9Fu8",
+    authDomain: "live-chat-room-7bf3a.firebaseapp.com",
+    projectId: "live-chat-room-7bf3a",
+    storageBucket: "live-chat-room-7bf3a.appspot.com",
+    messagingSenderId: "561500397861",
+    appId: "1:561500397861:web:2cb02c17b5790c1f69facc",
+    measurementId: "G-Q2Y6CTLW5K",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp };
